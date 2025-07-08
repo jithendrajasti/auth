c51c1fc3e77a16cfe5a5cdbad67c5e982114591a
jithendrajasti <jithendrajasti11@gmail.com>
2025-07-08 14:49:45 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const AppContext=createContext();
 
 export const AppContextProvider=(props)=>{
-    const backendUrl=import.meta.env.VITE_BACKEND_URL;
+    const backendUrl="https://auth-37ye.onrender.com";
     const [isLoggedin,setIsLoggedin]=useState(false);
     const[userData,setUserData]=useState({});
     const getUserData=async()=>{
